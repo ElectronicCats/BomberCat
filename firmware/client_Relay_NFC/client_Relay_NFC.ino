c8866b08c8bb063e064bc9c6bcd01de35243f388
Sabas <s@electroniccats.com>
2022-07-15 21:36:33 -0500

    add magspoof code

--- firmware/client_Relay_NFC/client_Relay_NFC.ino
+++ firmware/client_Relay_NFC/client_Relay_NFC.ino
@@ -46,7 +46,32 @@ String clientId = "BomberCatClient-001";
 
 #define L1         (LED_BUILTIN)  //LED1 indicates activity
 
-#define NPIN       (2) //NFC Button
+#define PIN_A      (18)   //MagSpoof-1   GP19  
+#define PIN_B      (19)   //MagSpoof     GP19 
+
+#define NPIN       (17) //Button
+
+#define CLOCK_US   (500)
+
+#define BETWEEN_ZERO (53) // 53 zeros between track1 & 2
+
+#define TRACKS (2)
+
+// consts get stored in ram as we don't adjust them
+char tracks[128][2];
+
+char revTrack[41];
+
+const int sublen[] = {
+  32, 48, 48
+};
+
+const int bitlen[] = {
+  7, 5, 5
+};
+
+unsigned int curTrack = 0;
+int dir;
 
 WiFiClient espClient;
 int status = WL_IDLE_STATUS;
@@ -79,8 +104,150 @@ boolean detectCardFlag = false;
 uint8_t ppdol[255] = {0x80, 0xA8, 0x00, 0x00, 0x02, 0x83, 0x00};
 
 /*****************
-       NFC
+       MAGSPOOF
  *****************/
+// send a single bit out
+void playBit(int sendBit) {
+  dir ^= 1;
+  digitalWrite(PIN_A, dir);
+  digitalWrite(PIN_B, !dir);
+  delayMicroseconds(CLOCK_US);
+
+  if (sendBit) {
+    dir ^= 1;
+    digitalWrite(PIN_A, dir);
+    digitalWrite(PIN_B, !dir);
+  }
+  delayMicroseconds(CLOCK_US);
+}
+
+// when reversing
+void reverseTrack(int track) {
+  int i = 0;
+  track--; // index 0
+  dir = 0;
+
+  while (revTrack[i++] != '?');
+  i--;
+  while (i--)
+    for (int j = bitlen[track] - 1; j >= 0; j--)
+      playBit((revTrack[i] >> j) & 1);
+}
+
+// plays out a full track, calculating CRCs and LRC
+void playTrack(int track) {
+  int tmp, crc, lrc = 0;
+  dir = 0;
+  track--; // index 0
+
+  // enable H-bridge and LED
+  //digitalWrite(ENABLE_PIN, HIGH);
+
+  // First put out a bunch of leading zeros.
+  for (int i = 0; i < 25; i++)
+    playBit(0);
+
+  for (int i = 0; tracks[track][i] != '?'; i++)
+  {
+    crc = 1;
+    tmp = tracks[track][i] - sublen[track];
+
+    for (int j = 0; j < bitlen[track] - 1; j++)
+    {
+      crc ^= tmp & 1;
+      lrc ^= (tmp & 1) << j;
+      playBit(tmp & 1);
+      tmp >>= 1;
+    }
+    playBit(crc);
+  }
+
+  // finish calculating and send last "byte" (LRC)
+  tmp = lrc;
+  crc = 1;
+  for (int j = 0; j < bitlen[track] - 1; j++)
+  {
+    crc ^= tmp & 1;
+    playBit(tmp & 1);
+    tmp >>= 1;
+  }
+  playBit(crc);
+
+  // if track 1, play 2nd track in reverse (like swiping back?)
+  if (track == 0)
+  {
+    // if track 1, also play track 2 in reverse
+    // zeros in between
+    for (int i = 0; i < BETWEEN_ZERO; i++)
+      playBit(0);
+
+    // send second track in reverse
+    reverseTrack(2);
+  }
+
+  // finish with 0's
+  for (int i = 0; i < 5 * 5; i++)
+    playBit(0);
+
+  digitalWrite(PIN_A, LOW);
+  digitalWrite(PIN_B, LOW);
+
+}
+
+// stores track for reverse usage later
+void storeRevTrack(int track) {
+  int i, tmp, crc, lrc = 0;
+  track--; // index 0
+  dir = 0;
+
+
+  for (i = 0; tracks[track][i] != '?'; i++)
+  {
+    crc = 1;
+    tmp = tracks[track][i] - sublen[track];
+
+    for (int j = 0; j < bitlen[track] - 1; j++)
+    {
+      crc ^= tmp & 1;
+      lrc ^= (tmp & 1) << j;
+      tmp & 1 ?
+      (revTrack[i] |= 1 << j) :
+      (revTrack[i] &= ~(1 << j));
+      tmp >>= 1;
+    }
+    crc ?
+    (revTrack[i] |= 1 << 4) :
+    (revTrack[i] &= ~(1 << 4));
+  }
+
+  // finish calculating and send last "byte" (LRC)
+  tmp = lrc;
+  crc = 1;
+  for (int j = 0; j < bitlen[track] - 1; j++)
+  {
+    crc ^= tmp & 1;
+    tmp & 1 ?
+    (revTrack[i] |= 1 << j) :
+    (revTrack[i] &= ~(1 << j));
+    tmp >>= 1;
+  }
+  crc ?
+  (revTrack[i] |= 1 << 4) :
+  (revTrack[i] &= ~(1 << 4));
+
+  i++;
+  revTrack[i] = '?';
+}
+
+void magspoof() {
+  //if (digitalRead(MPIN) == 0) {
+    Serial.println("Activating MagSpoof...");
+    playTrack(1 + (curTrack++ % 2));
+    blink(L1, 150, 3);
+    delay(400);
+ // }
+}
+
 void resetMode() { //Reset the configuration mode after each reading
   Serial.println("Reset...");
   if (nfc.connectNCI()) { //Wake up the board
