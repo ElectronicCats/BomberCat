2369362a85af975683360ce724a9674a29a98a3b
Sabas <s@electroniccats.com>
2022-07-30 23:55:30 -0500

    Update SerialPassthrough

--- firmware/ESP32SerialPassthroughFlash/ESP32SerialPassthroughFlash.ino
+++ firmware/ESP32SerialPassthroughFlash/ESP32SerialPassthroughFlash.ino
@@ -1,15 +1,15 @@
 /*
-   EspSerialPassthrough v1.0
-   ESP8266 / ESP-01 Serial Passthrough for flashing using Arduino SAMD MCU
-   This Arduino sketch allows you to send AT commands, watch bootloader messages, or flash an ESP-01/ESP8266 through a SAMD21/SAMD51 or similar MCU. 
+   ESP32SerialPassthroughFlash v1.0
+   ESP32 / NINA Serial Passthrough for flashing using Arduino MCU
+   This Arduino sketch allows you to send AT commands, watch bootloader messages, or flash an ESP32 through a RP2040 or similar MCU. 
    The MCU should be operating at 3.3V, if not, voltage level shifters must be used on the connections to ESP.
-   The program might also work on other ESPs, but have only been tested with ESP8266 in the form of a ESP-01 board.
+   The program might also work on other ESPs, but have only been tested with ESP32.
    
-   Developed and tested with a Arduino MKR board, SAMD21-based.
+   Developed and tested with a Arduino RP2040-based.
    The program might work on MCUs without native USB, but no guarantees! :-)
    Sending AT commands has been tested using the Arduino serial monitor, Visual Micro serial monitor and Putty in serial mode.
    The same goes for watching bootloader output.
-   Firmware upgrade/flashing has been done with the official ESP8266 Flash Download Tool from https://www.espressif.com/en/support/download/other-tools, 
+   Firmware upgrade/flashing has been done with the official ESP32 Flash Download Tool from https://www.espressif.com/en/support/download/other-tools, 
    with firmware downloaded from https://www.espressif.com/en/support/download/at
   Copyright 2021 Kåre Smith (Kaare Smith)
   Licensed under the Apache License, Version 2.0 (the "License");
@@ -60,5 +60,5 @@ void enterFlashMode() {
   delay(100);
   digitalWrite(NINA_RESETN, LOW);
 
-  // Now the ESP8266 should be in flashing mode
+  // Now the ESP32 should be in flashing mode
 }
