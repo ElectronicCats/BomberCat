5eeb4325834e2783011ac4ded7bb41476449af70
Sabas <s@electroniccats.com>
2022-09-12 23:33:43 -0500

    Add save config  wifi and mqtt in flash host

--- firmware/host_Relay_NFC/host_Relay_NFC.ino
+++ firmware/host_Relay_NFC/host_Relay_NFC.ino
@@ -26,21 +26,56 @@
   /************************************************************/
 
 #include "arduino_secrets.h"
-#include <ArduinoJson.h>
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <PubSubClient.h>
+#include <SerialCommand.h>
 #include "Electroniccats_PN7150.h"
 
 //#define DEBUG
+#define SERIALCOMMAND_HARDWAREONLY
 #define PERIOD 20000
 #define HOST 0
 
-// Update these with values suitable for your network.
+#include <FlashIAPBlockDevice.h>
+#include <TDBStore.h>
 
-const char* ssid = ssidName;
-const char* password = passWIFI;
-const char* mqtt_server = mqttServ;
+using namespace mbed;
+
+// Get limits of the In Application Program (IAP) flash, ie. the internal MCU flash.
+#include "FlashIAPLimits.h"
+auto iapLimits { getFlashIAPLimits() };
+
+// Create a block device on the available space of the FlashIAP
+FlashIAPBlockDevice blockDevice(iapLimits.start_address, iapLimits.available_size);
+
+// Create a key-value store on the Flash IAP block device
+TDBStore store(&blockDevice);
+
+SerialCommand SCmd;
+
+float fwVersion = 0.2;
+
+// Update these with values suitable for your network.
+char mqtt_server[] = mqttServ;
+char ssid[255] = SECRET_SSID;        // your network SSID (name)
+char pass[255] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+auto result = 0;
+// An example key name for the stats on the store
+const char statsKey[] { "stats" };
+
+// Dummy sketch stats data for demonstration purposes
+struct SketchStats {
+  char ssidStore[255];
+  char passwordStore[255];
+  char mqttStore[255];
+};
+
+// Previous stats
+SketchStats previousStats;
+
+boolean flagWifi, flagMqtt, flagStore = false;
 
 char outTopic[] = "RelayHost#";
 char inTopic[] = "RelayClient#";
@@ -81,9 +116,39 @@ uint8_t ppse[255];
 boolean detectCardFlag = false;
 
 /*****************
+   File System
+ ********************/
+// Retrieve SketchStats from the key-value store
+int getSketchStats(const char* key, SketchStats* stats)
+{
+  // Retrieve key-value info
+  TDBStore::info_t info;
+  auto result = store.get_info(key, &info);
+
+  if (result == MBED_ERROR_ITEM_NOT_FOUND)
+    return result;
+
+  // Allocate space for the value
+  uint8_t buffer[info.size] {};
+  size_t actual_size;
+
+  // Get the value
+  result = store.get(key, buffer, sizeof(buffer), &actual_size);
+  if (result != MBED_SUCCESS)
+    return result;
+
+  memcpy(stats, buffer, sizeof(SketchStats));
+  return result;
+}
+
+// Store a SketchStats to the the k/v store
+int setSketchStats(const char* key, SketchStats stats)
+{
+  return store.set(key, reinterpret_cast<uint8_t*>(&stats), sizeof(SketchStats), 0);
+}
+/*****************
        NFC
  *****************/
-
 void resetMode() { //Reset the configuration mode after each reading
   #ifdef DEBUG
   Serial.println("Reset...");
@@ -254,10 +319,141 @@ void mifarevisa() {
   }
   //detectCardFlag = false;
 }
+/*****************
+       WIFI
+ *****************/
+void setup_wifi() {
+  if (!flagStore) {
+    char *arg;
+    arg = SCmd.next();    // Get the next argument from the SerialCommand object buffer
+    if (arg != NULL) {
+      strcpy(ssid, arg);
+      Serial.print("First argument was: ");
+      Serial.println(ssid);
+    }
+    else {
+      Serial.println("No arguments for WiFi");
+    }
+
+    arg = SCmd.next();
+    if (arg != NULL) {
+      strcpy(pass, arg);
+      Serial.print("Second argument was: ");
+      Serial.println(pass);
+    }
+    else {
+      Serial.println("No second argument for pass");
+    }
+  }
+  else {
+    result = getSketchStats(statsKey, &previousStats);
+
+    strcpy(ssid, previousStats.ssidStore);
+    strcpy(pass, previousStats.passwordStore);
+  }
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  if (!flagStore) {
+    result = getSketchStats(statsKey, &previousStats);
+
+    strcpy(previousStats.ssidStore, ssid);
+    strcpy(previousStats.passwordStore, pass);
+
+    result = setSketchStats(statsKey, previousStats);
+
+    if (result == MBED_SUCCESS) {
+      Serial.println("Save WiFiSetup in Stats Flash");
+      Serial.print("\tSSID: ");
+      Serial.println(previousStats.ssidStore);
+      Serial.print("\tWiFiPass: ");
+      Serial.println(previousStats.passwordStore);
+      Serial.print("\tMQTT Server: ");
+      Serial.println(previousStats.mqttStore);
+
+    } else {
+      Serial.println("Error while saving to key-value store");
+      while (true);
+    }
+  }
+  flagWifi = true;
+}
 
 /*****************
        MQTT
  *****************/
+
+void setup_mqtt() {
+  if (!flagStore) {
+    char *arg;
+    arg = SCmd.next();    // Get the next argument from the SerialCommand object buffer
+    if (arg != NULL) {    // As long as it existed, take it
+      strcpy(mqtt_server, arg);
+      Serial.print("MQTT Server: ");
+      Serial.println(mqtt_server);
+    }
+    else {
+      Serial.println("No arguments for MQTTServer");
+    }
+  }
+  Serial.print("Connecting MQTT to ");
+  Serial.println(mqtt_server);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  if (!flagStore) {
+    result = getSketchStats(statsKey, &previousStats);
+
+    strcpy(previousStats.mqttStore, mqtt_server);
+
+
+    result = setSketchStats(statsKey, previousStats);
+
+    if (result == MBED_SUCCESS) {
+      Serial.println("Save MQTTSetup in Stats Flash");
+      Serial.print("\tSSID: ");
+      Serial.println(previousStats.ssidStore);
+      Serial.print("\tWiFiPass: ");
+      Serial.println(previousStats.passwordStore);
+      Serial.print("\tMQTT Server: ");
+      Serial.println(previousStats.mqttStore);
+    } else {
+      Serial.println("Error while saving to key-value store");
+      while (true);
+    }
+  }
+  flagMqtt = true;
+  Serial.println("connected MQTT");
+}
+
 //Callback MQTT suscribe to inTopic from RelayClient
 void callback(char* topic, byte * payload, unsigned int length) {
   #ifdef DEBUG
@@ -321,41 +517,6 @@ void reconnect() {
   }
 }
 
-/*****************
-       WIFI
- *****************/
-void setup_wifi() {
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
-    while (true);
-  }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, password);
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-  }
-}
-
 void blink(int pin, int msdelay, int times) {
   for (int i = 0; i < times; i++) {
     digitalWrite(pin, HIGH);
@@ -373,12 +534,63 @@ void setup() {
   #ifdef DEBUG
   while (!Serial);
   #endif
+  
   mode = 1;
   resetMode();
 
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  // Get limits of the the internal flash of the microcontroller
+  auto [flashSize, startAddress, iapSize] = getFlashIAPLimits();
+
+  Serial.print("Flash Size: ");
+  Serial.print(flashSize / 1024.0 / 1024.0);
+  Serial.println(" MB");
+  Serial.print("FlashIAP Start Address: 0x");
+  Serial.println(startAddress, HEX);
+  Serial.print("FlashIAP Size: ");
+  Serial.print(iapSize / 1024.0 / 1024.0);
+  Serial.println(" MB");
+
+  // Create a block device on the available space of the flash
+  FlashIAPBlockDevice blockDevice(startAddress, iapSize);
+
+  // Initialize the Flash IAP block device and print the memory layout
+  blockDevice.init();
+
+  // Initialize the key-value store
+  Serial.print("Initializing TDBStore: ");
+  auto result = store.init();
+  Serial.println(result == MBED_SUCCESS ? "OK" : "Failed");
+  if (result != MBED_SUCCESS)
+    while (true); // Stop the sketch if an error occurs
+
+  // Get previous run stats from the key-value store
+  Serial.println("Retrieving Sketch Stats");
+  result = getSketchStats(statsKey, &previousStats);
+
+  if (result == MBED_SUCCESS) {
+    Serial.println("Previous Setup Stats WiFi and MQTT");
+    Serial.print("\tSSID: ");
+    Serial.println(previousStats.ssidStore);
+    Serial.print("\tWiFiPass: ");
+    Serial.println(previousStats.passwordStore);
+    Serial.print("\tMQTT Server: ");
+    Serial.println(previousStats.mqttStore);
+    flagStore = true;
+  } else if (result == MBED_ERROR_ITEM_NOT_FOUND) {
+    Serial.println("No previous data for wifi and mqtt was found.");
+    Serial.println("Run setup_wifi command.");
+    Serial.println("Run setup_mqtt command.");
+  } else {
+    Serial.println("Error reading from key-value store.");
+    while (true);
+  }
+
+  if (flagStore == true) {
+    setup_wifi();
+  }
+  if ((flagWifi == true) && (flagStore == true)) {
+    setup_mqtt();
+  }
 
   // blink to show we started up
   blink(L1, 200, 6);
@@ -389,16 +601,36 @@ void setup() {
   outTopic[9] = HOST + 48;
   
   Serial.println(outTopic);
-  reconnect();
+  if (flagMqtt == 1) {
+    reconnect();
+  }
+
+  Serial.println("BomberCat, yes Sir!");
+  Serial.println("Host Relay NFC");
+  Serial.println("Welcome to the BomberCat CLI " + String(fwVersion, 1) + "v\n");
+  Serial.println("Type help to get the available commands.");
+  Serial.println("Electronic Cats ® 2022");
+
+  // Setup callbacks for SerialCommand commands
+  SCmd.addCommand("help", help);
+  SCmd.addCommand("setup_wifi", setup_wifi);
+  SCmd.addCommand("setup_mqtt", setup_mqtt);
+
+  SCmd.setDefaultHandler(unrecognized);  // Handler for command that isn't matched  (says "What?")
 }
 
 void loop() { // Main loop
 /*
-  if (!client.connected()) {
-    reconnect();
+  if (flagMqtt == true) {
+    if (!client.connected()) {
+      reconnect();
+    }
   }
 */
-  client.loop();
+  if (flagMqtt == 1) {
+    // procesa mensajes MQTT
+    client.loop();
+  }
 
   if((millis() - tiempo) > PERIOD && host_selected){
     // RESET host connection
@@ -409,4 +641,19 @@ void loop() { // Main loop
     apdulen = 0;
     reconnect();
   }
+}
+
+void help() {
+  Serial.println("Fw version: " + String(fwVersion, 1) + "v");
+  Serial.println("\tConfiguration commands:");
+  Serial.println("\tsetup_wifi");
+  Serial.println("\tsetup_mqtt");
+
+  Serial.println("Monitor commands:");
+  Serial.println("\tget_hs");
+  Serial.println("..help");
+}
+// This gets set as the default handler, and gets called when no other command matches.
+void unrecognized(const char *command) {
+  Serial.println("Command not found, type help to get the valid commands");
 }
