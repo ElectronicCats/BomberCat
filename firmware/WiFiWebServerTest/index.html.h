65551dfc47c0c9f383ed0d7671abd1bc219cb756
Deimos <deimoshall@gmail.com>
2023-04-12 21:42:56 -0600

    test: alert using javascript

--- firmware/WiFiWebServerTest/index.html.h
+++ firmware/WiFiWebServerTest/index.html.h
@@ -2,35 +2,39 @@
 
 String index_html = R"=====(
 <!DOCTYPE html>
-<html lang="es">
-    <head>
-        <title>WiFi Web Server Test</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta charset="utf-8">
-        <style>
-            )=====" + styles_css + R"=====(
-        </style>
-    </head>
-    <body>
-        <header>
-            <div class="header-container">
-                <img src="./Logo-100_100.png" alt="logo" class="logo">
-                <h1 class="logoTitle">NFC Copy Cat</h1>
-            </div>
-        </header>
-        <section class="signin">
-            <div class="form-container">
-                <h2 class="title">Sign in</h2>
-                <p class="subtitle">Enter your username and password.</p>
-                <form action="/" class="form">
-                    <p>
-                        <input type="username" required id="username" placeholder="       Username" class="input input-username">
-                        <input type="password" required id="password" placeholder="       Password" class="input input-password">
-                        <input type="submit" value="Sign in" class="primary-button signin-button">
-                    </p>
-                </form>
-            </div>
-        </section>
-    </body>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        )=====" + styles_css + R"=====(
+    </style>
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <img src="./Logo-100_100.png" alt="logo" class="logo">
+            <h1 class="logoTitle">NFC Copy Cat</h1>
+        </div>
+    </header>
+    <section class="signin">
+        <div class="form-container">
+            <h2 class="title">Sign in</h2>
+            <p class="subtitle">Enter your username and password.</p>
+            <form action="/" class="form">
+                <p>
+                    <input type="username" required id="username" placeholder="       Username" class="input input-username">
+                    <input type="password" required id="password" placeholder="       Password" class="input input-password">
+                    <input type="submit" value="Sign in" class="primary-button signin-button">
+                </p>
+            </form>
+        </div>
+    </section>
+    <script>
+        alert('Hello')
+    </script>
+</body>
 </html>
 )=====";
