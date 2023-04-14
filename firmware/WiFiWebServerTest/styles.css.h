3e6946de5ca128ba35797b5b965d33414a87cc9d
Deimos <deimoshall@gmail.com>
2023-04-14 12:40:37 -0600

    feat: add the rest of the styles

--- firmware/WiFiWebServerTest/styles.css.h
+++ firmware/WiFiWebServerTest/styles.css.h
@@ -172,4 +172,155 @@ input::-webkit-outer-spin-button, input::-webkit-inner-spin-button {
     font-size: var(--md);
     color: var(--white);
 }
+
+.ok-button {
+    margin-left: 25px;
+}
+
+.header-home {
+    margin: 0 auto;
+    width: 100%;
+    height: 180px;
+    display: flex;
+    flex-direction: row;
+    place-items: center;
+    background-color: var(--main-logo-background);
+    color: var(--white);
+    margin-bottom: 80px;
+}
+
+.logoP {
+    margin: 20px;
+}
+
+.logoPTitle{
+    vertical-align: middle;
+    text-align: left;
+    font-size: var(--md);
+    font-weight: 700;
+}
+
+.menu-container {
+    margin: 0 auto;
+    display: grid;
+    width: 500px;
+    height: 340px;
+    border-radius: 50px;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    place-items: center;
+    align-items: center;
+    text-align: center;
+    align-content: center;
+}
+
+a {
+    margin-top: 32px;
+    text-decoration: none;
+    color: var(--text);
+    font-size: var(--md);
+    font-weight: 700;
+    place-content: center;
+}
+
+.footer-home {
+    margin-top: 72px;
+    position: fixed;
+    width: 100%;
+    height: 60px;
+    bottom: 0;
+    border-top: 1px solid var(--nav-bar);
+    background-color: var(--white);
+    place-items: center;
+    align-content: center;
+    display: none;
+}
+
+.config-header {
+    margin: 54px auto;
+    width: 500px;
+    text-align: center;
+    font-size: var(--lg);
+    font-weight: normal;
+    margin-bottom: 64px;
+}
+
+.card-container {
+    margin: 0 auto;
+    margin-top: 40px;
+    display: grid;
+    width: 500px;
+    grid-auto-flow: column;
+    grid-template-columns: 1fr 100px;
+    justify-content: end;
+    align-items: center;  
+}
+
+.input-card, .text-card {
+   grid-area: header;
+   width: 280px;
+}
+
+.input-date, .text-date {
+    grid-area: main1;
+    width: 125px;
+    margin-right: 8px;
+}
+
+.text-date, .text-code {
+    font-size: var(--xsm);
+}
+
+.input-code, .text-code {
+    grid-area: main2;
+    width: 125px;
+    margin-left: 8px;
+}
+
+.send-button, .read-button {
+    grid-area: footer;
+    width: 200px;
+}
+
+.mags-container, .nfc-container {
+    margin-top: 40px;
+    display: flex;
+    width: 500px;
+    margin: 0 auto;
+    flex-direction: column;
+    align-items: center;
+}
+
+.card-form, .read-form {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-areas: "header header" "main1 main2" "footer footer";
+    align-items: center;
+    justify-items: center;
+    margin-bottom: 90px;
+}
+
+@media (max-width: 640px) {
+    .logo{
+        display: block;
+    }
+    .logoTitle{
+        display: block;
+    }  
+    .menu-container {
+        width: 320px;
+    }
+    .footer-home{
+        display: block;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    .card-container {
+        margin: 0 auto;
+        width: 400px;
+        justify-items: center;
+    }
+}
 )=====";
