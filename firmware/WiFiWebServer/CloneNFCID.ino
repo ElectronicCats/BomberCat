494aa1a4c3b3d5597defaecfb06d74f3dd926b40
Deimos <deimoshall@gmail.com>
2023-06-19 16:17:08 -0600

    refactor: replace apdus with dummy data

--- firmware/WiFiWebServer/CloneNFCID.ino
+++ firmware/WiFiWebServer/CloneNFCID.ino
@@ -5,7 +5,7 @@ void printData(uint8_t* buff, uint8_t lenbuffer, uint8_t cmd) {
   else if (cmd == 2)
     Serial.print("\nReader command: ");
   else if (cmd == 3)
-    Serial.print("\nHunter Cat answer: ");
+    Serial.print("\nBomberCat answer: ");
   else
     Serial.print("\nCard answer: ");
 
@@ -19,41 +19,16 @@ void printData(uint8_t* buff, uint8_t lenbuffer, uint8_t cmd) {
 void emulateNFCID() {
   mode = 2;
   resetMode();
-  memcpy(&card[0], last, sizeof(last));
-  memcpy(&card[4], token, sizeof(token));
-  memcpy(&card[23], statusapdu, sizeof(statusapdu));
-
-  uint8_t* apdus2[] = {ppsea, visaa, processinga, card, finished, finished};
-  uint8_t apdusLen2[] = {sizeof(ppsea), sizeof(visaa), sizeof(processinga), sizeof(card), sizeof(finished), sizeof(finished)};
-
-  unsigned long lastTime = millis();
   uint8_t counter = 0;
 
-  // while(true) {
-  //   nfc.CardModeSend(apdus2[0], apdusLen2[0]);
-  //   printData(apdus2[0], apdusLen2[0], 3);
-  // }
-
-  for (uint8_t i = 0; i < 6; i++) {
-    // Serial.println("i = " + String(i));
-
-    if (nfc.CardModeReceive(Cmd, &CmdSize) == 0) {  // Data in buffer?
-
-      while ((CmdSize < 2) && (Cmd[0] != 0x00)) {
-      }
-
+  while (true) {
+    if (nfc.CardModeReceive(Cmd, &CmdSize) == 0) {  // Receive command from reader
       printData(Cmd, CmdSize, 1);
+      // printData(data, sizeof(data), 3);
+      nfc.CardModeSend(data, sizeof(data)); // Emulate the NFCID
 
-      nfc.CardModeSend(apdus2[i], apdusLen2[i]);
-
-      printData(apdus2[i], apdusLen2[i], 3);
       Serial.println("counter = " + String(++counter));
-      // counter++;
-
-      // if (++counter == 6) break;
-
-    } else {
-      i--;
+      // if (counter == 10) break;
     }
   }
 }
