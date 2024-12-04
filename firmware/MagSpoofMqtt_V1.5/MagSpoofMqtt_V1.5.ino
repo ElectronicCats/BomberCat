3a8c1869c8d663995cc7beeccc76a633cd8a06cc
Emmanuel1-hub <180690050+Emmanue1-hub@users.noreply.github.com>
2024-12-04 11:54:13 -0600

    feat: V1.5 of the MagSpoofMQTT example was added.

--- firmware/MagSpoofMqtt_V1.5/MagSpoofMqtt_V1.5.ino
+++ firmware/MagSpoofMqtt_V1.5/MagSpoofMqtt_V1.5.ino
@@ -1,50 +1,23 @@
-/************************************************************
-  Example for send Magstripe with MagSpoof via MQTT version for BomberCat
-  by Jaz, Electronic Cats (https://electroniccats.com/)
-  by Salvador Mendoza (salmg.net)
-  by Andres Sabas
-  Date: 17/05/2022
-
-  This example demonstrates how to use BomberCat by Electronic Cats
-  https://github.com/ElectronicCats/BomberCat
-
-  Development environment specifics:
-  IDE: Arduino 1.8.19
-  Hardware Platform:
-  BomberCat
-  - RP2040
-
-  Thanks Wallee for support this project open source https://en.wallee.com/
-
-  Electronic Cats invests time and resources providing this open source code,
-  please support Electronic Cats and open-source hardware by purchasing
-  products from Electronic Cats!
-
-  This code is beerware; if you see me (or any other Electronic Cats
-  member) at the local, and you've found our code helpful,
-  please buy us a round!
-  Distributed as-is; no warranty is given.
+/*
+The changes to this code were minumum and were mainly in the Void callback.
 */
+
 #include "arduino_secrets.h"
 #include <ArduinoJson.h>
 #include <WiFiNINA.h>
 #include <PubSubClient.h>
-// Update these with values suitable for your network.
 
 const char* ssid = ssidName;
 const char* password = passWIFI;
 const char* mqtt_server = mqttServ;
 
-#define L1         (LED_BUILTIN)  //LED1 indicates activity
-
-#define PIN_A      (6)   //MagSpoof-1  
-#define PIN_B      (7)   //MagSpoof
-
-#define NPIN       (5) //Button
+#define L1         (LED_BUILTIN)  // LED1 indicates activity
+#define PIN_A      (6)            // MagSpoof-1  
+#define PIN_B      (7)            // MagSpoof
+#define NPIN       (5)            // Button
 #define CLOCK_US   (500)
 
-#define BETWEEN_ZERO (53) // 53 zeros between track1 & 2
-
+#define BETWEEN_ZERO (53) // 53 zeros between track1 and 2
 #define TRACKS (2)
 #define DEBUGCAT
 
@@ -52,24 +25,17 @@ unsigned long myTime1;
 unsigned long myTime2;
 unsigned long result;
 
-// consts get stored in ram as we don't adjust them
 char tracks[2][128];
-
 char revTrack[41];
 
-const int sublen[] = {
-  32, 48, 48
-};
-const int bitlen[] = {
-  7, 5, 5
-};
+const int sublen[] = { 32, 48, 48 };
+const int bitlen[] = { 7, 5, 5 };
 
 unsigned int curTrack = 0;
 int dir;
 
 WiFiClient espClient;
 int status = WL_IDLE_STATUS;
-
 PubSubClient client(espClient);
 
 unsigned long lastMsg = 0;
@@ -81,16 +47,12 @@ int value = 0;
        WIFI
  *****************/
 void setup_wifi() {
-
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
     while (true);
   }
 
@@ -99,64 +61,79 @@ void setup_wifi() {
     Serial.println("Please upgrade the firmware");
   }
 
-  // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, password);
+    delay(5000);
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    if (status == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+    }
   }
 }
 
+/*
+The changes were to restrict the messages sent through the Mag topic and that can activate the MagSpoof mode. Now the code
+verify if the message has the form of the track1 and track2, if it is not the case, then it descards it.
+*/
+
 void callback(char* topic, byte* payload, unsigned int length) {
+  if (strcmp(topic, "Mag") == 0) {
 
-  int i, j;
-  j = 0;
-  for (i = 0; i < 255; i++) {
-        if (payload[i] == '?' && j == 0) {
-          tracks[0][i] = payload[i];
-          j = i;
-          tracks[0][i + 1] = NULL;
-        }
-        if (j == 0) {
-          tracks[0][i] = payload[i];
-        }
-        else {
-          tracks[1][i - j] = payload[i + 1];
-          if (payload[i + 1] == '?') {
-            tracks[1][i - j + 1] = NULL;
-            break;
-          }
-        }
-      }
+    // Converts the payload into a character array    
+    char message[length + 1];
+    strncpy(message, (char*)payload, length);
+    message[length] = '\0';
 
-  magspoof();
+    // Look these specific characters in the payload. 
+    char* questionMark = strchr(message, '?');
+    char* semicolon = strchr(message, ';');
 
+    // Verify that it contains % at the beginning and ? at the end, as well as the position of ? and ;
+    if (message[0] != '%' || message[length - 1] != '?' || questionMark == NULL || semicolon == NULL || questionMark >= semicolon) {
+      Serial.println("Invalid message received in the Mag topic. Ignoring...");
+      return;
+    }
+
+    // Track 1: from the beginning to '?'
+    int track1Length = questionMark - message + 1;
+    strncpy(tracks[0], message, track1Length);
+    tracks[0][track1Length] = '\0';
+
+    // Track 2: from ';' to the end, including ';'
+    int track2Start = semicolon - message;
+    int track2End = length - track2Start;
+    strncpy(tracks[1], message + track2Start, track2End);
+    tracks[1][track2End] = '\0';
+
+    // Shows the tracks in the Serial monitor
+    Serial.println();
+    Serial.println("Track 1:");
+    Serial.println(tracks[0]);
+    Serial.println("Track 2:");
+    Serial.println(tracks[1]);
+
+    magspoof();
+  }
 }
+
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
     String clientId = "ESPClient-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
       client.publish("Mag", "hello i'm here");
-      // ... and resubscribe
       client.subscribe("Mag");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -167,7 +144,7 @@ void setup() {
   pinMode(PIN_B, OUTPUT);
   pinMode(L1, OUTPUT);
   pinMode(NPIN, INPUT_PULLUP);
-  pinMode(13, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(13, OUTPUT);
 
   setup_wifi();
   client.setServer(mqtt_server, 1883);
@@ -175,7 +152,6 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  // blink to show we started up
   blink(L1, 200, 2);
   Serial.println("Ready MQTT MagSpoof");
 }
@@ -189,7 +165,6 @@ void blink(int pin, int msdelay, int times) {
   }
 }
 
-// send a single bit out
 void playBit(int sendBit) {
   dir ^= 1;
   digitalWrite(PIN_A, dir);
@@ -204,10 +179,9 @@ void playBit(int sendBit) {
   delayMicroseconds(CLOCK_US);
 }
 
-// when reversing
 void reverseTrack(int track) {
   int i = 0;
-  track--; // index 0
+  track--;
   dir = 0;
 
   while (revTrack[i++] != '?');
@@ -217,23 +191,19 @@ void reverseTrack(int track) {
       playBit((revTrack[i] >> j) & 1);
 }
 
-// plays out a full track, calculating CRCs and LRC
 void playTrack(int track) {
   int tmp, crc, lrc = 0;
   dir = 0;
-  track--; // index 0
+  track--;
 
-  // First put out a bunch of leading zeros.
   for (int i = 0; i < 25; i++)
     playBit(0);
 
-  for (int i = 0; tracks[track][i] != '?'; i++)
-  {
+  for (int i = 0; tracks[track][i] != '?'; i++) {
     crc = 1;
     tmp = tracks[track][i] - sublen[track];
 
-    for (int j = 0; j < bitlen[track] - 1; j++)
-    {
+    for (int j = 0; j < bitlen[track] - 1; j++) {
       crc ^= tmp & 1;
       lrc ^= (tmp & 1) << j;
       playBit(tmp & 1);
@@ -242,78 +212,55 @@ void playTrack(int track) {
     playBit(crc);
   }
 
-  // finish calculating and send last "byte" (LRC)
   tmp = lrc;
   crc = 1;
-  for (int j = 0; j < bitlen[track] - 1; j++)
-  {
+  for (int j = 0; j < bitlen[track] - 1; j++) {
     crc ^= tmp & 1;
     playBit(tmp & 1);
     tmp >>= 1;
   }
   playBit(crc);
 
-  // if track 1, play 2nd track in reverse (like swiping back?)
-  if (track == 0)
-  {
-    // if track 1, also play track 2 in reverse
-    // zeros in between
+  if (track == 0) {
     for (int i = 0; i < BETWEEN_ZERO; i++)
       playBit(0);
-
-    // send second track in reverse
     reverseTrack(2);
   }
 
-  // finish with 0's
   for (int i = 0; i < 5 * 5; i++)
     playBit(0);
 
   digitalWrite(PIN_A, LOW);
   digitalWrite(PIN_B, LOW);
-
 }
 
-// stores track for reverse usage later
 void storeRevTrack(int track) {
   int i, tmp, crc, lrc = 0;
-  track--; // index 0
+  track--;
   dir = 0;
 
-
-  for (i = 0; tracks[track][i] != '?'; i++)
-  {
+  for (i = 0; tracks[track][i] != '?'; i++) {
     crc = 1;
     tmp = tracks[track][i] - sublen[track];
 
-    for (int j = 0; j < bitlen[track] - 1; j++)
-    {
+    for (int j = 0; j < bitlen[track] - 1; j++) {
       crc ^= tmp & 1;
       lrc ^= (tmp & 1) << j;
-      tmp & 1 ?
-      (revTrack[i] |= 1 << j) :
-      (revTrack[i] &= ~(1 << j));
+      tmp & 1 ? (revTrack[i] |= 1 << j) : (revTrack[i] &= ~(1 << j));
       tmp >>= 1;
     }
-    crc ?
-    (revTrack[i] |= 1 << 4) :
-    (revTrack[i] &= ~(1 << 4));
+    crc ? (revTrack[i] |= 1 << 4) : (revTrack[i] &= ~(1 << 4));
   }
-
-  // finish calculating and send last "byte" (LRC)
   tmp = lrc;
   crc = 1;
-  for (int j = 0; j < bitlen[track] - 1; j++)
-  {
+  for (int j = 0; j < bitlen[track] - 1; j++) {
     crc ^= tmp & 1;
-    tmp & 1 ?
-    (revTrack[i] |= 1 << j) :
-    (revTrack[i] &= ~(1 << j));
+
+    
+    tmp & 1 ? (revTrack[i] |= 1 << j) : (revTrack[i] &= ~(1 << j));
     tmp >>= 1;
   }
-  crc ?
-  (revTrack[i] |= 1 << 4) :
-  (revTrack[i] &= ~(1 << 4));
+  crc ? (revTrack[i] |= 1 << 4) : (revTrack[i] &= ~(1 << 4));
 
   i++;
   revTrack[i] = '?';
@@ -331,5 +278,4 @@ void loop() {
     reconnect();
   }
   client.loop();
-
 }
